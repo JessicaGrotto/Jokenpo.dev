c8141e8d9b65b9281e4711405866dbb10d1b31bb
JessicaGrotto <jessicalgrotto@gmail.com>
2023-09-04 20:46:31 +0200

    finalizacao

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
 
 const playHuman = (humanChoice) => {  
     
@@ -7,7 +13,6 @@ const playHuman = (humanChoice) => {
 const playMachine = () => {
     const choices = ['rock', 'paper', 'scissors']
     const randomNumber = Math.floor(Math.random() * 3) 
-
     return choices[randomNumber]     
 }
 
@@ -15,4 +20,20 @@ const playTheGame = (human, machine) => {
 
     console.log("Humano: "+ human + " Máquina: "+ machine)
 
+    if(human === machine){
+        result.innerHTML = "Deu empate!"
+    } else if(
+        (human === 'paper' && machine ==='rock') || 
+        (human === 'rock' && machine ==='scissors') || 
+        (human === 'scissors' && machine ==='paper') ){
+            humanScoreNumber++
+            humanScore.innerHTML = humanScoreNumber
+            result.innerHTML = "Você ganhou!" 
+    } else{ 
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = "Você perdeu!"
+    }
+
 }
+
